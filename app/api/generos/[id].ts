0edf8aae4be47233a9cf527ecd3230f07b006b93
MonsterEvil0666 <gabriel.y2011@gmail.com>
2024-11-17 22:09:53 -0300

    Corrigido metodo de deleção de Generos

--- app/api/generos/[id].ts
+++ app/api/generos/[id].ts
@@ -1,8 +1,7 @@
 import { NextResponse } from 'next/server';
 import clientPromise from '../../../lib/mongodb';
-import { NextRequest } from 'next/server';
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
   try {
     const client = await clientPromise;
     const db = client.db();
@@ -28,7 +27,7 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
   }
 }
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
   try {
     const client = await clientPromise;
     const db = client.db();
